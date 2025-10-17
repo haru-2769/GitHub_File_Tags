9390487a6bd723517c09afa84b8cbd01b3c2767b
NanoCha <u213805j@ecs.osaka-u.ac.jp>
2025-10-17 17:17:18 +0900

    adapt prettier

--- src/ts/background.ts
+++ src/ts/background.ts
@@ -1,7 +1,7 @@
-console.log("Service Worker: GitHub File Tags Background Service is Running.");
+console.log('Service Worker: GitHub File Tags Background Service is Running.');
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log(
-    "Service Worker: Extension installed successfully. Version 0.1.0."
+    'Service Worker: Extension installed successfully. Version 0.1.0.',
   );
 });
--- src/ts/content.ts
+++ src/ts/content.ts
@@ -1,40 +1,40 @@
-import "../css/tag.css";
-import { observe } from "selector-observer";
+import '../css/tag.css';
+import { observe } from 'selector-observer';
 
 function processCommitLink(commitLinkEl: HTMLElement): void {
-  const rowElement = commitLinkEl.closest("tr.react-directory-row");
+  const rowElement = commitLinkEl.closest('tr.react-directory-row');
   if (!rowElement) {
     return;
   }
 
   const filenameLink = rowElement.querySelector<HTMLAnchorElement>(
-    "td.react-directory-row-name-cell-large-screen a"
+    'td.react-directory-row-name-cell-large-screen a',
   );
 
-  if (!filenameLink || !filenameLink.textContent?.trim().endsWith(".md")) {
+  if (!filenameLink || !filenameLink.textContent?.trim().endsWith('.md')) {
     return;
   }
 
   const commitCell = rowElement.querySelector<HTMLTableCellElement>(
-    "td.react-directory-row-commit-cell"
+    'td.react-directory-row-commit-cell',
   );
 
   if (!commitCell) {
     return;
   }
 
-  commitCell.innerHTML = "";
-  const docsTag = document.createElement("span");
-  docsTag.className = "docs-tag";
-  docsTag.textContent = "docs";
+  commitCell.innerHTML = '';
+  const docsTag = document.createElement('span');
+  docsTag.className = 'docs-tag';
+  docsTag.textContent = 'docs';
   commitCell.appendChild(docsTag);
 }
 
 observe(
-  "td.react-directory-row-commit-cell .react-directory-commit-message a",
+  'td.react-directory-row-commit-cell .react-directory-commit-message a',
   {
     add(commitLinkEl) {
       processCommitLink(commitLinkEl as HTMLElement);
     },
-  }
+  },
 );
